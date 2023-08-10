ae9dda3c32c2d3ed329a143e668aef22fa4383a4
Ajay <aji478514@gmail.com>
2023-08-10 21:24:27 +0530

    URL shorten feature added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const validurl = require('valid-url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +20,30 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const url = {};
+let shorturld = 1;
+
+app.post("/api/shorturl", function (req, res) {
+  const longurl = req.body.url;
+  if (!validurl.isUri(longurl)) {
+    return res.status(400).json({ error: "invalid url" });
+  }
+  const shorturl = `${req.protocol}://${req.get('host')}/api/shorturl/${shorturld}`;
+  url[shorturld] = longurl;
+  shorturld++;
+  res.json({ shorturl: shorturl, originalurl: longurl });
+});
+
+app.get("/api/shorturl/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const longurl = url[id];
+  if (url.hasOwnProperty(id)) {
+    return res.redirect(301, longurl);
+  } else {
+    return res.status(404).json({ error: "url not found" });
+  }
 });
 
 app.listen(port, function() {
